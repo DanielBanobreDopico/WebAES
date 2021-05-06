35ff1ca49f055dfdcd749ca23e3581cd585568bd
Daniel Banore Dopico <daniel.banobre.dopico@protonmail.com>
2021-05-06 20:06:23 +0200

    Add padding for hidding secret length

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,14 @@
 			- https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto
 		Dan Allison
 			- https://gist.github.com/danallison/3ec9d5314788b337b682
+		https://stackoverflow.com/users/633183/thank-you
+			- https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 	*/
 
+	const maxSecretLength = 446;
 	let action = "encrypt";
-	let passphrase, salt, key, exportedKey, counter, secret, encrypted;
+	let passphrase, salt, key, counter, encrypted, encryptedObject, padding;
+	let secret = '';
 
 	function keySeed () {
 		let enc = new TextEncoder();
@@ -26,7 +30,7 @@
 		key = await window.crypto.subtle.deriveKey(
 			{
 			"name": "PBKDF2",
-			salt: salt,
+			salt: crypto.getRandomValues(new Uint8Array(16)),
 			"iterations": 100000,
 			"hash": "SHA-512"
 			},
@@ -35,30 +39,8 @@
 			true,
 			[ "encrypt", "decrypt" ]
 		);
-		exportedKey = await crypto.subtle.exportKey('jwk',key);
-		encrypt();
-	}
 
-	function generateKey () {
-		window.crypto.subtle.generateKey(
-		  {
-			name: "AES-CTR",
-			length: 256,
-		  },
-		  true,
-		  ["encrypt", "decrypt"]
-		)
-		.then(
-			(k)=>{
-				key = k;
-				crypto.subtle.exportKey('jwk',key).then(
-					(p)=>{
-						console.log(JSON.stringify(p))
-						exportedKey = JSON.stringify(p);
-					}
-				)
-			}
-		)
+		encrypt();
 	}
 
 	function arrayBufferToBase64( buffer ) {
@@ -68,12 +50,16 @@
 		for (var i = 0; i < len; i++) {
 			binary += String.fromCharCode( bytes[i] );
 		}
-		return window.btoa( binary );
+		return btoa( binary );
 	}
 	
 	async function encrypt () {
+		const saltedSecret = {
+			secret,
+			padding,
+		}
 		const encoder = new TextEncoder();
-		const encoded = encoder.encode(secret);
+		const encoded = encoder.encode(JSON.stringify(saltedSecret));
 		let cipher;
 		try {
 			cipher = await window.crypto.subtle.encrypt(
@@ -83,7 +69,7 @@
 					length: 64
 				},
 				key,
-				encoded
+				encoded,
 			);
 		} catch (err) {
 			console.error(err);
@@ -108,12 +94,25 @@
 		setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
 	}
 
-	salt = crypto.getRandomValues(new Uint8Array(16));
+	function dec2hex (dec) {
+		return dec.toString(16).padStart(2, "0")
+	}
+
 	counter = crypto.getRandomValues(new Uint8Array(16));
-	generateKey();
+
+	$: if (secret) {
+		let length = (maxSecretLength - secret.length) / 2;
+		let binaryPadding = crypto.getRandomValues(new Uint8Array(length));
+		padding = Array.from(binaryPadding, dec2hex).join('');
+		padding = padding + '='.repeat(maxSecretLength-(padding.length+secret.length));
+	}
 
 	$:{
-		
+		encryptedObject = {
+			counter: btoa(counter),
+			encrypted,
+		};
+		//console.log(encrypted);
 	}
 
 </script>
@@ -125,11 +124,11 @@
 
 	{#if action==="encrypt"}
 
+	<h3>Frase de paso:</h3>
+	<input disabled="{counter ? false : true}" type="text" bind:value="{passphrase}" on:input="{derivateKey}"/>
+
 	<h3>Texto a cifrar:</h3>
 	<textarea disabled="{key ? false : true}" maxlength="446" bind:value={secret} on:input={encrypt}></textarea>
-
-	<h3>Frase de paso:</h3>
-	<input type="text" bind:value="{passphrase}" on:input="{derivateKey}"/>
 
 	<h3>Información cifrada:</h3>
 	<textarea disabled bind:value="{encrypted}"></textarea>
